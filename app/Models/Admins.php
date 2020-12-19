e63eb82e1a8df69757d486867294847426c42050
Ghulam Mustafa <mustafaji@gmail.com>
2020-12-19 23:19:35 +0100

    worked on jwt, admin model, and admins controller

--- app/Models/Admins.php
+++ app/Models/Admins.php
@@ -8,5 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Admins extends Model
 {
     use HasFactory;
-    
+    protected $table = 'admins';
+    protected $fillable = ['name','email','password','created_at','updated_at'];
+    protected $hidden = ['password','remember_token'];
 }
