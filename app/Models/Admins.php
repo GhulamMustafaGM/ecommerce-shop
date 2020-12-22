a74da7757f387c33c68212da829b17ecf9ebca86
Ghulam Mustafa <mustafaji@gmail.com>
2020-12-22 01:12:08 +0100

    worked on modilfy and login frontend -> eCommerce with Laravel and React JS

--- app/Models/Admins.php
+++ app/Models/Admins.php
@@ -5,10 +5,32 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Admins extends Model
+class Admins extends Authenticatable implements JWTSubject
 {
-    use HasFactory;
+    use Notifiable;
+
+    // Rest omitted for brevity
     protected $table = 'admins';
     protected $fillable = ['name','email','password','created_at','updated_at'];
     protected $hidden = ['password','remember_token'];
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
+    
