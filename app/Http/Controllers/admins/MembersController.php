21a17ce5b4dea1c870775807e08ce49cd067014a
Ghulam Mustafa <mustafaji@gmail.com>
2020-12-30 10:16:17 +0100

    workded on add users backend

---
+++ app/Http/Controllers/admins/MembersController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers\Admins;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MembersController extends Controller
+{
+    //
+    use uploadphoto;
+    use ItemRules;
+
+    public function addItem(Request $request, $id) {
+
+        $rules = $this->ItemsRules();
+        $validator = Validator::make($request->all(), $rules);
+
+        if($validator->fails()) {
+            return response()->json(['error at validation'], 400);
+        }
+
+        //import from trait(uploadphoto)
+        
+        $fileName=$this->uploadphoto($request->file('photo'), 'images/items');
+
+        $items=Items::create([
+            'name' => $request->get('name'),
+            'description' => $request->get('description'),
+            'status' => $request->get('status'),
+            'price' => $request->get('price'),
+            'date' => now(),
+            'approve' => 1,
+            'photo' => $fileName,
+            'admins_id' => $id
+        ]
+        );
+            return response()->json(compact('items'));
+    }
+
+    public function getItem() {
+        $items = Items::orderBy('id', 'desc')->paginate(5);
+        return response()->json(compact('items'));
+    }
+
+    public function editItem($id) {
+        $items = Items::find($id);
+        return response()->json(compact('items'));
+    }
+
+    public function updateItem() {
+
+        $rules = $this->ItemsRules();
+        $validator = Validator::make($request->all(), $rules);
+        if($validator->fails()) {
+            return response()->json(['error at validation'], 400);
+        }
+            $fileName=$this->uploadphoto($request->file('photo'), 'images/items');
+
+            $items=Items::find($id);
+
+            $items->name=$request->name;
+            $items->description=$request->description;
+            $items->status=$request->status;
+            $items->price=$request->price;
+            $items->photo=$fileName;
+
+            $items->save();
+
+            return response()->json(compact('items'));
+    }
+
+    public function deleteItem($id) {
+        $items = Items::find($id);
+        $items->delete();
+    }
+}
+
---
+++ routes/api.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admins\AdminsController;
+use App\Http\Controllers\Admins\ItemsController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+    ########################## Login #########################################
+
+Route::group(['prefix' => 'admins','namespace'=>'Admins'], function () {
+    Route::post('login', [AdminsController::class,'adminsLogin']);
+        
+    });
+
+    ########################## getauthadmin #########################################
+
+    Route::group(['prefix' => 'admins','namespace'=>'Admins', 'middleware'=>'adminsRoutes','jwt.auth'], function () {
+        Route::get('authadmin', [AdminsController::class,'getAuthenticatedAdmin']);
+            
+        });
+
+    ######################################## items ##########################################
+
+    Route::group(['prefix' => 'admins','namespace'=>'Admins', 'middleware'=>'adminsRoutes','jwt.auth'], function () {
+        Route::post('add/items{id}', [ItemsController::class,'addItem']);
+        Route::get('get/items', [ItemsController::class,'getItem']);
+        Route::get('edit/items{id}', [ItemsController::class,'editItem']);
+        Route::post('update/items{id}', [ItemsController::class,'updateItem']);
+        Route::delete('delete/items{id}', [ItemsController::class,'deleteItem']);
+            
+        });
+
